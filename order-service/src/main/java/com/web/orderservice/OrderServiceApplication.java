fb03bcde10450badca0b4e0b0ddd63029d71a57e
Rashed <rashedulislam8741@yahoo.com>
2021-11-02 00:03:42 +0600

    authenticate order service with gateway

--- order-service/src/main/java/com/web/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/web/orderservice/OrderServiceApplication.java
@@ -4,12 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-
-import feign.RequestInterceptor;
-import feign.RequestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -20,16 +14,14 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 	
-	@Bean
-	public RequestInterceptor requestInterceptor() {
-		return new RequestInterceptor() {
-			
-			@Override
-			public void apply(RequestTemplate template) {
-				JwtAuthenticationToken token = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-				template.header("Authorization", "Bearer " + token.getToken().getTokenValue());
-			}
-		};
-	}
-
+	/*
+	 * @Bean public RequestInterceptor requestTokenBearerInterceptor() { return new
+	 * RequestInterceptor() {
+	 * 
+	 * @Override public void apply(RequestTemplate template) {
+	 * JwtAuthenticationToken token = (JwtAuthenticationToken)
+	 * SecurityContextHolder.getContext().getAuthentication();
+	 * template.header("Authorization", "Bearer " +
+	 * token.getToken().getTokenValue()); } }; }
+	 */
 }
